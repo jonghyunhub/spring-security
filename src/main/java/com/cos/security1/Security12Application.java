390f59f40d7167c28e02cce83a256b5ff628c12d
chujonghyun <chuwhdgus@gmail.com>
2022-10-31 22:16:15 +0900

    first commit

---
+++ src/main/java/com/cos/security1/Security12Application.java
@@ -0,0 +1,13 @@
+package com.cos.security1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security12Application.class, args);
+	}
+
+}
